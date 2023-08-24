ff7940c7334e276cd4e5fec18aa20a0755265df8
JJ Brown <jj.brown.255@proton.me>
2023-08-24 10:39:12 -0600

    improved printout

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,16 @@ use crate::scan::*;
 
 #[async_std::main]
 async fn main() {
-    match scan_once_for_devices().await {
-        Ok(addrs) => println!("Found cast devices: {:?}", addrs),
-        Err(e) => eprintln!("error polling: {:?}", e),
+    let _devices = match scan_once_for_devices().await {
+        Ok(devices) => { report_devices(&devices); devices }
+        Err(e) => { eprintln!("error polling: {:?}", e); Vec::new() },
+    };
+}
+
+fn report_devices(devices: &[FoundDevice]) {
+    println!("Found devices:");
+    let width = devices.iter().map(|d| d.name.len()).max().unwrap_or(0);
+    for device in devices {
+        println!("\t{:width$}\t{}", device.name, device.addr, width = width)
     }
 }
